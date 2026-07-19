9c3ef6bd488fe734e29a06428f621b1d787c1219
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-19 14:34:54 -0500

    Expose ARM64 list extend runtime

--- asmpython/_backends/arm64/runtime_manifest.py
+++ asmpython/_backends/arm64/runtime_manifest.py
@@ -26,6 +26,7 @@ RUNTIME_SLICES = (
         frozenset(
             {
                 "_abi_list_append",
+                "_abi_list_extend",
                 "_abi_list_pop",
                 "_abi_list_reverse",
                 "_abi_new_list",
