45a1784b42d0aaf6c2f97270b9a6c38337adcf83
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 17:00:37 -0500

    test: add static data descriptor regression

---
+++ tests/cases/468_static_data_descriptor.py
@@ -0,0 +1,33 @@
+# expect:
+# 7
+# 42
+# value
+
+
+class Descriptor:
+    def __init__(self, default):
+        self.default = default
+        self.name = ""
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner=None):
+        return instance.values.get(self.name, self.default)
+
+    def __set__(self, instance, value):
+        instance.values[self.name] = value
+
+
+class Item:
+    value = Descriptor(7)
+
+    def __init__(self):
+        self.values = {}
+
+
+item = Item()
+print(item.value)
+item.value = 42
+print(item.value)
+print(Descriptor(0).name or "value")
