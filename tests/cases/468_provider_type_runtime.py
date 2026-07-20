cd2e916f8e895ec9f07d5d2be96e38afadc7e1b7
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 18:44:19 -0500

    Add provider runtime type regression

---
+++ tests/cases/468_provider_type_runtime.py
@@ -0,0 +1,32 @@
+# expect:
+# 1
+# 0
+# SomniaProvider
+# 1
+
+
+class Provider:
+    runtime_realms = ("server", "client")
+
+    @classmethod
+    def supports_realm(cls, realm: str) -> bool:
+        return realm in cls.runtime_realms
+
+
+class ServerProvider(Provider):
+    runtime_realms = ("server",)
+
+
+class ClientProvider(Provider):
+    runtime_realms = ("client",)
+
+
+def starts_with_somnia(value) -> bool:
+    return value.startswith("somnia.")
+
+
+provider_types = (ServerProvider, ClientProvider)
+print(provider_types[0].supports_realm("server"))
+print(provider_types[1].supports_realm("server"))
+print(str(Provider))
+print(starts_with_somnia("somnia.Scene"))
