f94d38230bd52604f69796afc721e519454156db
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 18:18:47 -0500

    Install return annotation precedence

--- asmpython/_compiler/__init__.py
+++ asmpython/_compiler/__init__.py
@@ -20,5 +20,6 @@ from . import live_definition_compat_fixes as _live_definition_compat_fixes
 from . import empty_collection_compat_fixes as _empty_collection_compat_fixes
 from . import ordered_flow_compat_fixes as _ordered_flow_compat_fixes
 from . import descriptor_precedence_compat_fixes as _descriptor_precedence_compat_fixes
+from . import return_annotation_precedence_compat_fixes as _return_annotation_precedence_compat_fixes
 
 __all__ = ["__version__"]
