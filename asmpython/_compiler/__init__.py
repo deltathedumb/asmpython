a470e5fe220f74dd2309da90ba887b5830fb082d
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 17:39:00 -0500

    Load type-parameter specialization pass

--- asmpython/_compiler/__init__.py
+++ asmpython/_compiler/__init__.py
@@ -9,6 +9,7 @@ from . import linux_runtime_fixes as _linux_runtime_fixes
 from . import language_compat_fixes as _language_compat_fixes
 from . import metaclass_compat_fixes as _metaclass_compat_fixes
 from . import class_registry_compat_fixes as _class_registry_compat_fixes
+from . import type_parameter_compat_fixes as _type_parameter_compat_fixes
 from . import program_compat_fixes as _program_compat_fixes
 from . import analysis_compat_fixes as _analysis_compat_fixes
 
