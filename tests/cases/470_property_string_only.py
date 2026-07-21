60be13bf27becd7f999c3b11cda996f288dfd4b7
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 20:28:37 -0500

    Add direct string property regression

---
+++ tests/cases/470_property_string_only.py
@@ -0,0 +1,26 @@
+# expect:
+# somnia.Root
+
+
+class Registry:
+    def __init__(self):
+        self.names = {"Root": "somnia.Root"}
+
+    def type_name(self, value):
+        return self.names.get(value.name, value.name)
+
+
+REGISTRY = Registry()
+
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def type_name(self):
+        return REGISTRY.type_name(self)
+
+
+root = Node("Root")
+print(root.type_name)
