6f664c7886ac4e943a24335ce9ea2a0ecf5f5228
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 20:07:21 -0500

    Load direct static/class method call lowering

--- asmpython/_compiler/__init__.py
+++ asmpython/_compiler/__init__.py
@@ -24,5 +24,6 @@ from . import return_annotation_precedence_compat_fixes as _return_annotation_pr
 from . import class_value_compat_fixes as _class_value_compat_fixes
 from . import dynamic_value_compat_fixes as _dynamic_value_compat_fixes
 from . import concrete_specialization_compat_fixes as _concrete_specialization_compat_fixes
+from . import static_method_call_compat_fixes as _static_method_call_compat_fixes
 
 __all__ = ["__version__"]
