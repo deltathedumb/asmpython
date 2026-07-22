0242d23554a38e00d0f48bf30616933422cf709f
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-21 20:33:19 -0500

    Expose compatibility extras namespace

---
+++ asmpython/extras/__init__.py
@@ -0,0 +1,6 @@
+"""Compatibility namespace for the API now exported directly by :mod:`asmpython`.
+
+Prefer ``from asmpython import Public, access`` in new source.
+"""
+from ._api import *
+from ._api import __all__
