b3d2b050f8b7d23d7bc0a3c1e54fb79e5e9c9bcc
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-21 20:44:06 -0500

    Scaffold AVR backend package

---
+++ asmpython/_backends/avr/__init__.py
@@ -0,0 +1,8 @@
+"""Scaffold for the planned AVR backend."""
+
+from ..scaffolds import SCAFFOLD_BACKENDS
+
+__module_backend__ = SCAFFOLD_BACKENDS["avr"]
+backend = __module_backend__
+
+__all__ = ["__module_backend__", "backend"]
